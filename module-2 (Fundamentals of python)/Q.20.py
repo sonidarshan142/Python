25e25b3fe67ccb067b331e31ad51504f39a26fd6
Darshan mandaliya <159875296+sonidarshan142@users.noreply.github.com>
2024-08-13 11:06:36 +0530

    Update Q.20.py

--- module-2 (Fundamentals of python)/Q.20.py
+++ module-2 (Fundamentals of python)/Q.20.py
@@ -3,10 +3,10 @@ Write a Python function that takes a list of words and returns the length
 of the longest one.
 """
 
-max_length = 0
+maxlength = 0
 words = input("Enter a string: ").split()
 
 for word in words:
-    max_length = max(max_length, len(word))
+    maxlength = max(maxlength, len(word))
 
-print(max_length)
+print(maxlength)
