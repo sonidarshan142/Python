fe1299feb47820ab035ae78fe158fbe213f74ea4
Darshan mandaliya <159875296+sonidarshan142@users.noreply.github.com>
2024-08-13 11:00:43 +0530

    Update Q.17.py

--- module-2 (Fundamentals of python)/Q.17.py
+++ module-2 (Fundamentals of python)/Q.17.py
@@ -7,10 +7,10 @@ str1 = input("Enter the first string: ")
 str2 = input("Enter the second string: ")
 
 # swapping
-new_str1 = str2[:2] + str1[2:]
-new_str2 = str1[:2] + str2[2:]
+newstr1 = str2[:2] + str1[2:]
+newstr2 = str1[:2] + str2[2:]
 
 #combine
-result = new_str1 + " " + new_str2
+result = newstr1 + " " + newstr2
 print(result)
 
