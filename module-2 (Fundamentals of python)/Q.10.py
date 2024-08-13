bf77afa1ee849422dd271ff18adaf6263b15288a
Darshan mandaliya <159875296+sonidarshan142@users.noreply.github.com>
2024-08-13 12:53:28 +0530

    Update Q.10.py

--- module-2 (Fundamentals of python)/Q.10.py
+++ module-2 (Fundamentals of python)/Q.10.py
@@ -5,7 +5,15 @@ values are equal or their sum or difference is 5
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
+result = False
 
-ans = num1 == num2 or (num1 - num2) == 5 or num1 + num2 == 5
+if num1 == num2:
+    result = True
+elif abs(num1 - num2) == 5:
+    result = True
+elif abs(num1 + num2) == 5:
+    result = True
+else:
+    print(result)
 
-print(ans)
+
