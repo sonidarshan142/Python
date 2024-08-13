f3a8937f73cfb3e4b50330028085e3c14f662d6a
Darshan mandaliya <159875296+sonidarshan142@users.noreply.github.com>
2024-08-13 13:51:42 +0530

    Update Q.23.py

--- module-2 (Fundamentals of python)/Q.23.py
+++ module-2 (Fundamentals of python)/Q.23.py
@@ -1,14 +1,11 @@
 #Write a Python function to insert a string in the middle of a string.
 
-test_str = "Hello world"
-mid_str = "the"
-print("The original string is :"+str(test_str))
-mid_str = input("Enter a middle string: ")
+string = input("Enter a string : ")
+insert = input("Enter a middle string : ")
 
-temp = test_str.split()
-mid_pos = len(temp)// 2
+middleindex = len(string) // 2
+result = string[:middleindex] + insert + string[middleindex:]
 
-res = " ".join(temp[:mid_pos]+[mid_str]+temp[mid_pos:])
+print(result)
 
-print(+str(res))
 
