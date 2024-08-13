e60bcddf188cd4ca5e0a5d1ab9c00e1113854603
Darshan mandaliya <159875296+sonidarshan142@users.noreply.github.com>
2024-08-13 12:58:45 +0530

    Update Q.16.py

--- module-2 (Fundamentals of python)/Q.16.py
+++ module-2 (Fundamentals of python)/Q.16.py
@@ -1,8 +1,7 @@
 """Write a Python program to count the occurrences of each word in a
 given sentence
 """
-
-a1 = "This is a sentence with some words repeated This is"
+a1 = input("Enter char : ")
 a1 = a1.lower()
 # Split the sentence into words
 words = a1.split()
