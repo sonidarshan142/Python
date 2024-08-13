3d623cdca2a961624b474b28da6abc6ef7b92122
Darshan mandaliya <159875296+sonidarshan142@users.noreply.github.com>
2024-08-13 12:38:48 +0530

    Update Q.2.py

--- module-2 (Fundamentals of python)/Q.2.py
+++ module-2 (Fundamentals of python)/Q.2.py
@@ -1,13 +1,10 @@
 #Write a Python program to get the Factorial number of given number.
 
-num = int(input("Enter number :"))
-factorial = 1
+num = int(input("Enter the number : "))
+print(num)
+fact = 1
 
-if num == 0:
-    print("The factorial of 0 is 1")
-else:
-    for i in range(1, num + 1):
-        factorial *= num
-
-print(f"The factorial of {num} is {factorial}")
-
+for i in range(1, num + 1):
+        fact *= i
+    
+print(f"The factotial of {num} is {fact}")
